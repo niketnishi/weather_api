8943ba389a30f501c1414c72b3550bcd93efd368
Niket Nishi <niketnishi@gmail.com>
2018-09-03 22:24:33 +0530

    partial commit

--- static/js/weather_api.js
+++ static/js/weather_api.js
@@ -6,15 +6,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function findWeather(eventObj) {
     if (eventObj.keyCode == 13){
-        let cityName = document.querySelector('#city_name').value;
+        let cityName = document.querySelector('#city_name');
+        alert(`${cityName}`);
         if (cityName.length > 0) {
             var xhttp = new XMLHttpRequest();
-            if (this.readyState == 4 && this.status == 200) {
-                document.querySelector('#climate_info').innerHTML = this.responseText;
-                alert("you are in");
-            } else {
-                document.querySelector('#climate_info').innerHTML = "Sorry your request can not be processed right now."
-            }
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.querySelector('#climate_info').innerHTML = this.responseText;
+                    alert("you are in");
+                } else {
+                    document.querySelector('#climate_info').innerHTML = "Sorry your request can not be processed right now."
+                }
+            };
+            xhttp.open("GET", "api.openweathermap.org/data/2.5/weather?q=London", true);
+            xhttp.send();
         } else {
             alert('Please enter a city name to proceed!');
         }
